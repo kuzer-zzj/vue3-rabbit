91983d39ba354c1ff1d9892d9e90b118bf06ea69
zzj <156321781@qq.com>
2023-12-21 16:36:23 +0800

    吸顶导航

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -29,51 +29,58 @@ getCategoryList()
   </header>
 </template>
 
-<style lang="scss" scoped>
+<style scoped lang='scss'>
 .app-header {
   background: #fff;
+
   .container {
     display: flex;
     align-items: center;
-    .logo {
-      width: 200px;
-      a {
-        display: block;
-        height: 132px;
-        width: 100%;
-        text-indent: -999px;
-        background: url(@/assets/images/logo.png) no-repeat center 18px /
-          contain;
-      }
+  }
+
+  .logo {
+    width: 200px;
+
+    a {
+      display: block;
+      height: 132px;
+      width: 100%;
+      text-indent: -9999px;
+      background: url('@/assets/images/logo.png') no-repeat center 18px / contain;
     }
   }
+
   .app-header-nav {
     width: 820px;
     display: flex;
     padding-left: 40px;
     position: relative;
     z-index: 998;
+  
     li {
       margin-right: 40px;
+      width: 38px;
       text-align: center;
-      width: 48px;
-
+  
       a {
-        height: 32px;
-        display: inline-block;
         font-size: 16px;
         line-height: 32px;
+        height: 32px;
+        display: inline-block;
+  
         &:hover {
           color: $xtxColor;
           border-bottom: 1px solid $xtxColor;
         }
       }
+  
       .active {
         color: $xtxColor;
         border-bottom: 1px solid $xtxColor;
       }
     }
   }
+
   .search {
     width: 170px;
     height: 32px;
@@ -85,9 +92,10 @@ getCategoryList()
       font-size: 18px;
       margin-left: 5px;
     }
+
     input {
-      padding-left: 5px;
       width: 140px;
+      padding-left: 5px;
       color: #666;
     }
   }
