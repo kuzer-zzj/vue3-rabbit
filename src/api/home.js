73db6d270a68e1384a384f74d401899b5b407d06
zzj <156321781@qq.com>
2023-12-26 15:01:14 +0800

    banner

---
+++ src/api/home.js
@@ -0,0 +1,5 @@
+import http from "@/utils/http";
+
+export const bannerListReq =() => http({
+    url: 'home/banner'
+  })
--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,3 +1,47 @@
+<script setup>
+import {bannerListReq} from "@/api/home"
+import {ref,onMounted} from "vue"
+
+const bannerList = ref([])
+
+const getBannerList = async () => {
+    const res = await bannerListReq()
+    console.log(res)
+    bannerList.value = res.result
+}
+
+//dom加载结束调用getBannerList()
+onMounted(() => getBannerList())
+
+
+</script>
+
+
+
 <template>
-    轮播图
+  
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+
+
+<style scoped lang='scss'>
+.home-banner{
+    width: 1240px;
+    height: 500px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 98;
+    img{
+        width: 100%;
+        height: 500px;
+    }
+}
+</style>
